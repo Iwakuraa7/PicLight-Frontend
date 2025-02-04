16001f9ce7ea5cb6aa5277a7b0e2809f495e9359
Erlan_Esengeldiev <erlanesengeldiev7@gmail.com>
2025-02-04 17:52:11 +0600

    Adjust mockup

--- pages/ProfilePage.tsx
+++ pages/ProfilePage.tsx
@@ -1,14 +1,22 @@
+import React from 'react'
 import PhotoBlock from '../components/PhotoBlock'
+import styles from '../styles/ProfilePage.module.css'
 
 export default function ProfilePage()
 {
     const img = 'https://www.nylabone.com/-/media/project/oneweb/nylabone/images/dog101/10-intelligent-dog-breeds/golden-retriever-tongue-out.jpg?h=430&w=710&hash=7FEB820D235A44B76B271060E03572C7'
-    const photos = [img, img, img, img, img, img]
+    const imgObj = { id: 1, url: img }
+    const photos = [imgObj, imgObj, imgObj, imgObj, imgObj, imgObj, imgObj, imgObj, imgObj, imgObj, imgObj, imgObj]
 
     return(
-        <div>
+        <div className={styles['profilePageMain']}>
             <h1>ProfilePage</h1>
-            <PhotoBlock photos={photos}/>
+            <div className={styles['photoBlocksBox']}>
+                <PhotoBlock photos={photos} year={2021}/>
+                <PhotoBlock photos={photos} year={2022}/>
+                <PhotoBlock photos={photos} year={2023}/>
+                <PhotoBlock photos={photos} year={2024}/>
+            </div>
         </div>
     )
 }
