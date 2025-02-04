01bccee6950bc6cce912b59a342008e9c27afd68
Erlan_Esengeldiev <erlanesengeldiev7@gmail.com>
2025-02-04 17:51:26 +0600

    Fix types

--- components/PhotoBlock.tsx
+++ components/PhotoBlock.tsx
@@ -1,14 +1,27 @@
 import styles from '../styles/PhotoBlock.module.css'
 
-export default function PhotoBlock(props: string[])
+interface Photo {
+    id: number;
+    url: string;
+}
+
+interface PhotoBlockProps {
+    photos: Photo[];
+    year: number;
+}
+
+export default function PhotoBlock({ photos, year }: PhotoBlockProps)
 {
     return(
-        <div className={styles['mainPhotosBox']}>
-            {props.photos.map((photo: string) => (
-                <div className={styles['photoBox']}>
-                    <img src={photo}/>
-                </div>    
-            ))}
+        <div className={styles['mainPhotoBlock']}>
+            <h2>{year}</h2>
+            <div className={styles['mainPhotosBox']}>
+                {photos.map((photo: {id: number, url: string}) => (
+                    <div key={photo.id} className={styles['photoBox']}>
+                        <img src={photo.url} alt={`${photo.id}`}/>
+                    </div>    
+                ))}
+            </div>            
         </div>
     )
 }
